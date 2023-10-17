8e809f4588bbc4c4ccea7211da6419564efd11c4
Caroline Oliveira <anacdolima@gmail.com>
2023-10-16 21:10:11 -0300

    initial commit

---
+++ src/main/java/com/project/cinematek/CinematekApplication.java
@@ -0,0 +1,13 @@
+package com.project.cinematek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinematekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinematekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/cinematek/CinematekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cinematek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinematekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
